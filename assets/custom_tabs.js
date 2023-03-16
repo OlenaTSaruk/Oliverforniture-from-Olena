d4f35fae76b689e12c0488856e8df022ae44bb04
Olena Tsaruk <tsaruksiter@gmail.com>
2023-03-16 10:33:23 +0100

    Section_tub

---
+++ assets/custom_tabs.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  $(document).ready(function () {
+      function toggleTubs(e) {
+      e.preventDefault();
+      let parentItem = $(e.currentTarget).parent();
+      let elementActivatePosition = parentItem.index();
+
+      let tabHeadList = parentItem.parent().children();
+      let tabBodyList = parentItem.parent().parent().next().children().children();
+      let activateTab = (elements, activeIndex) => {
+        for (let item of elements) {
+          $(item).removeClass('active');
+          let elementPosition = $(item).index();
+          if (activeIndex == elementPosition) {
+            $(item).addClass('active');
+          }
+        }
+      };
+      activateTab(tabHeadList, elementActivatePosition);
+      activateTab(tabBodyList, elementActivatePosition);
+    }
+      $(document).on('click', '.list__link', function (e) {
+          return toggleTubs(e);
+        });
+  });
+});
